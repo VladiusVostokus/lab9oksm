83948dc8f0c8ec26074e8a6bf83be56232b1d2da
VladiusVostokus <kovalenkov222@gmail.com>
2023-06-19 09:10:37 +0300

    Initial client side

--- client.js
+++ client.js
@@ -2,9 +2,7 @@
 
 const net = require('net');
 
-console.log("hello world");
-
-const HOST = 127.0.0.1;
+const HOST = '127.0.0.1';
 const PORT = 2005;
 const PASSWORD = 'MyBirthdayDay';
 
@@ -13,6 +11,24 @@ const randomNum = (min,max) => Math.floor(Math.random() * (max - min) + min);
 const TURN = randomNum(-127,128);
 const SPEED = randomNum(0,256);
 const HEADLIGHTS = randomNum(0,2);
+
+const client = new net.Socket();
+
+client.connect(PORT, HOST, () => {
+    
+    console.log("CONNECTED TO THE SERVER");
+    
+    client.write(TURN);
+    client.write(SPEED);
+    client.write(HEADLIGHTS);
+});
+   
+
+client.on('close', () => {
+    console.log('DISCONNECTED FROM SERVER');
+});
+
+
     
 
 
